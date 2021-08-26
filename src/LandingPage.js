51cb5b11a917da7718a84b306e04474936d3a31f
matt <mattworthington51@gmail.com>
2021-08-26 13:54:12 +0100

    first pass on landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,15 +2,46 @@ import Typewriter from "typewriter-effect";
 import "./LandingPage.css";
 const LandingPage = () => {
   return (
-    <Typewriter
-      onInit={(typewriter) => {
-        typewriter
-          .typeString(
-            "<span class='name'>Matt Worthington</span><br/>FullstackDeveloper"
-          )
-          .start();
-      }}
-    />
+    <div className="landing-page">
+      <div className="code">
+        <h2>
+          <span className="const">const&nbsp;</span>
+          <span className="value">name&nbsp;</span> <span>=&nbsp;</span>
+          <span className="string">
+            &quot;Matt Worthington&quot;<span>;</span>
+          </span>
+        </h2>
+        <h2>
+          <span className="const">const&nbsp;</span>
+          <span className="value">title&nbsp;</span> <span>=&nbsp;</span>
+          <span className="string">
+            &quot;FullstackDeveloper&quot;<span>;</span>
+          </span>
+        </h2>
+
+        <Typewriter
+          delay={7000}
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(60)
+              .typeString(
+                "<span class='const'>const</span> <span class='value'>tagLine</span> = <span class='string'>`Let's have a chat about ${<span class='stringTemp'>yourCompanyName</span>}`</span>;"
+              )
+
+              .start();
+          }}
+        />
+      </div>
+      <div class="subtitle">
+        <p class="aboutButton">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          View my work<i class="gg-arrow-right-o"></i>
+        </p>
+      </div>
+    </div>
   );
 };
 export default LandingPage;
