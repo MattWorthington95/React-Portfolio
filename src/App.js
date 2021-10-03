f2535039eb82a0b261143e80449e558959edf893
matt <mattworthington51@gmail.com>
2021-10-03 16:23:32 +0100

    logged out resize listener

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import { useEffect, useState, useRef } from 'react';
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
-window.addEventListener('resize', function () {
-  'use strict';
-  window.location.reload();
-});
+// window.addEventListener('resize', function () {
+//   'use strict';
+//   window.location.reload();
+// });
 
 function App() {
   const prevScrollY = useRef(0);
